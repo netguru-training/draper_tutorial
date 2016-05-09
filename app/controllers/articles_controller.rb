2039425f800c434422a5950951ac88ebeca67d38
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-09 14:05:56 +0200

    Add possibility for showing and creating an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+  expose :article
+  expose :articles, -> { Article.all }
+
+  def index
+    articles
+  end
+
+  def create
+    if article.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
 end
