0ee3b55fd079fa5823760854f40dad6f32c80300
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-09 22:34:28 +0200

    Make cleanings into presenters

--- app/presenters/articles_presenter.rb
+++ app/presenters/articles_presenter.rb
@@ -1,10 +1,14 @@
-class ArticlePresenter < BasePresenter
+class ArticlesPresenter < BasePresenter
   def initialize(model, view)
     @model, @view = model, view
   end
 
   def h
     view
+  end
+
+  def decorate
+    model.map { |item| item.decorate }
   end
 
   private
