44127bc957dec999ab12ba3d658d8a7e5d066e8e
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-09 14:08:03 +0200

    Add possibility for validating models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+  belongs :author
+
+  validates :title, presence: true, length: { minimum: 8 }
+  validates :body, presence: true
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,3 @@
 class Author < ActiveRecord::Base
+  validates :first_name, :last_name, :publisher, presence: true, length: { minimum: 2 }
 end
