81cd085a55172d35dd6a6f2a2ea314b59f415203
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-09 14:19:27 +0200

    Add fields to the Article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509112743) do
+ActiveRecord::Schema.define(version: 20160509121850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20160509112743) do
   end
 
   create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "body"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "publisher"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
